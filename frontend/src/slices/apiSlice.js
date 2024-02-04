11c1b696f1f4485e18346d222ab7ff7f7204abe0
Kerem Akman <keremakman@Kerem-MacBook-Air.local>
2024-02-04 19:25:58 +0300

    update apiSlice to add backend api url

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,10 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://paha.onrender.com/api' || 'http://localhost:8080/api',
+  baseUrl:
+    'https://paha-api.vercel.app/api' ||
+    'https://paha-api-kakman82.vercel.app/api' ||
+    'http://localhost:8080/api',
   headers: {
     'Content-Type': 'application/json',
   },
