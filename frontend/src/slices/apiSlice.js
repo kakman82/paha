69491247a4dbe4ad872acec3c27809c3fe58adcf
Kerem Akman <keremakman@Kerem-MacBook-Air.local>
2024-02-04 20:40:45 +0300

    updated vite.config access-control-allow-origin

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,11 +2,13 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
   baseUrl:
-    'https://paha-api.vercel.app/api' ||
     'https://paha-api-kakman82.vercel.app/api' ||
+    'https://paha-api.vercel.app/api' ||
     'http://localhost:8080/api',
   headers: {
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin' : 'https://paha-api-kakman82.vercel.app/api' ||
+    'https://paha-api.vercel.app/api' ||
   },
   credentials: 'include',
 });
