91e07e90914c76313231303a060327e760e148ad
Kerem Akman <keremakman@Kerem-MacBook-Air.local>
2024-02-04 20:15:47 +0300

    updated server.js to add origin *

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ connectDB();
 // Middlewares;
 app.use(
   cors({
-    origin: origin,
+    origin: '*',
     preflightContinue: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
