580520bb72942a21caebfae41bffa5c56f2197fa
Kerem Akman <keremakman@Kerem-MacBook-Air.local>
2024-02-04 21:42:34 +0300

    "Access-Control-Allow-Origin", "*" addded in apiSlice

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -3,6 +3,7 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 const baseQuery = fetchBaseQuery({
   baseUrl: 'https://paha-api.vercel.app/api' || 'http://localhost:8080/api',
   headers: {
+    'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
   },
   credentials: 'include',
