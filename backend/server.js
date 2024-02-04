7641c2b1e09102f429826ea5b49bd0505c09d3b8
Kerem Akman <keremakman@Kerem-MacBook-Air.local>
2024-02-04 16:06:25 +0300

    origin: true

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,9 @@ connectDB();
 app.use(
   cors({
     // origin: process.env.CLIENT_URL || 'http://localhost:3000',
-    origin: '*',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'application/json'],
     credentials: true,
   })
 );
