e32b672fd3607fede985794749b5d68dec9fd2aa
Kerem Akman <keremakman@Kerem-MacBook-Air.local>
2024-03-10 02:44:15 +0300

    fixing navigate url after login

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
       const res = await logoutApiCall().unwrap();
       dispatch(clearCredentials());
       toast({ title: res.message, status: 'success' });
-      navigate('/auth/login');
+      navigate('/');
     } catch (err) {
       toast({
         title: err?.data?.message,
